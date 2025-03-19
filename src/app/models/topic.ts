16f39a37d8a7708e2e5f1d092b661a9d15bcc6da
m2gi-ettoumiy <yassir.ettoumi@etu.univ-grenoble-alpes.fr>
2025-03-19 14:40:45 +0100

    add optional fields in Topic

--- src/app/models/topic.ts
+++ src/app/models/topic.ts
@@ -4,9 +4,9 @@ export interface Topic {
   id: string;
   name: string;
   posts: Post[];
-  owner: string;
-  readers: string[];
-  writers: string[];
+  owner?: string;
+  readers?: string[];
+  writers?: string[];
 }
 
 export type Topics = Topic[];
