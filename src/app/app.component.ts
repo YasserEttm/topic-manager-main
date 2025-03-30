d86e18e319abf29ca46aacbf9ccf5bc81e90a7d7
Zaineb <Zaineb.Ikabour@etu.univ-grenoble-alpes.fr>
2025-03-31 00:33:52 +0200

    style pour les pages d'authentification et d'enregistrement

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonApp, IonRouterOutlet } from '@ionic/angular/standalone';
+import { addIcons } from 'ionicons';
 
 @Component({
   selector: 'app-root',
